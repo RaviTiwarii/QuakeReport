e8c8d1b579c9fd547d1fbd343afe2606a2dcfc6b
Ravi Tiwari <rvtwr123@gmail.com>
2018-10-07 15:13:48 +0530

    Show some fake earthquakes in a ListView

--- src/main/java/com/android/ravi/quakereport/EarthquakeActivity.java
+++ src/main/java/com/android/ravi/quakereport/EarthquakeActivity.java
@@ -2,6 +2,11 @@ package com.android.ravi.quakereport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class EarthquakeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView earthquakeListView = findViewById(R.id.list_view);
+
+        // Create a new {@link ArrayAdapter} of earthquakes
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, fakeEarthquakes());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        earthquakeListView.setAdapter(adapter);
+    }
+
+    private List<String> fakeEarthquakes() {
+        List<String> earthquakes = new ArrayList<>();
+        earthquakes.add("San Francisco");
+        earthquakes.add("London");
+        earthquakes.add("Tokyo");
+        earthquakes.add("Mexico City");
+        earthquakes.add("Moscow");
+        earthquakes.add("Rio de Janeiro");
+        earthquakes.add("Paris");
+        return earthquakes;
     }
 }
